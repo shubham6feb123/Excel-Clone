2ade97b17212def781ab07314456a5f89c025c87
shubham sharma <75255859+shubham6feb123@users.noreply.github.com>
2023-03-23 13:34:55 +0000

    Cut copy paste feaature addded

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-import {RenderCells,selectCell,getRowCol,updateCell} from "./util_function.js"
+import {RenderCells,selectCell,getRowCol,updateCell,cellProps,cellData,changeHeader,EmptySheet,loadSheet,renameSheet,deleteSheet,deleteCell} from "./util_function.js"
+
+export let selectedSheet = "sheet-1"
+let totalSheet = 1
+let lastlyAddedSheet = 1;
+let renameSheetSelected = ""
+let selectedCells = [];
+let cut = false;
 
 $(document).ready(function(){
 let input_cell_container = $(".input_cell_container")
@@ -20,38 +27,203 @@ for(let i=1;i<=N;i++){
   //rendering cells
 RenderCells(input_cell_container,N) 
 
- //making cells italic 
+ //making cells bold
 $('.icon_bold').click(function(){
  if($(this).hasClass('selected')){
-   updateCell("font-weight","")
+   updateCell("font-weight","",selectedSheet,true)
    // console.log('font-bold')
  }else{
    // console.log('font-bold')
-   updateCell("font-weight","bold")
+   updateCell("font-weight","bold",selectedSheet,false)
  }
 })
 
  //making cells italic 
 $('.icon_italic').click(function(){
  if($(this).hasClass('selected')){
-   updateCell("font-style","")
+   updateCell("font-style","",selectedSheet,true)
    // console.log('font-bold')
  }else{
    // console.log('font-bold')
-   updateCell("font-style","italic")
+   updateCell("font-style","italic",selectedSheet,false)
  }
 })
 
  //making cells underlined
   $('.icon_underline').click(function(){
  if($(this).hasClass('selected')){
-   updateCell("text-decoration","")
+   updateCell("text-decoration","",selectedSheet,true)
    // console.log('font-bold')
  }else{
    // console.log('font-bold')
-   updateCell("text-decoration","underline")
+   updateCell("text-decoration","underline",selectedSheet,false)
  }
 })
+
+//making cell left align
+  $('.icon_left').click(function(){
+     if(!$(this).hasClass('selected')){
+       updateCell('text-align','left',selectedSheet,false)
+     }
+  })
+
+  //making cell center align
+    $('.icon_center').click(function(){
+     if($(this).hasClass('selected')){
+       updateCell('text-align','left',selectedSheet,true)
+     }else{
+       updateCell('text-align','center',selectedSheet,false)
+     }
+  })
+
+    //making cell right align
+      $('.icon_right').click(function(){
+     if($(this).hasClass('selected')){
+       updateCell('text-align','left',selectedSheet,true)
+     }else{
+       updateCell('text-align','right',selectedSheet,false)
+     }
+  })
+
+  //handeling Cell's font family and size ---start----
+  $('.selector.font_family').change(function(e){
+      updateCell('font-family',$(this).val(),selectedSheet,false)
+    // console.log("this",e,font_family)
+  })
+
+  $('.selector.font_size').change(function(e){
+    // const font_size = e.target.value
+      updateCell('font-size',$(this).val()+"px",selectedSheet,false)
+    // console.log("this",e,font_size)
+  })  
+ // Cell's font family and size ---end----
+
+  //Cell's text storing ---Start---
+   $('.input_cell').blur(function(){
+     updateCell("text",$(this).text(),selectedSheet,false)
+   })
+   //Cell's text storing ---End---
+
+
+  //getting background and text color --START--
+  $('.color_picker').change(function(e){
+    if(e.target.name==="color_picker_background"){
+       updateCell("background-color",e.target.value,selectedSheet,true)
+    }
+    if(e.target.name==="color_picker_text"){
+        updateCell("color",e.target.value,selectedSheet,true)
+    }
+  })
+
+  //getting background and text color --END--
+
+  //Add Sheet --START--
+    $('.add_icon').click(function(){
+       EmptySheet(selectedSheet);
+       $('.sheet_tab.sheet_selected').removeClass('sheet_selected')
+      let sheetName = "sheet-"+(lastlyAddedSheet+1)
+      cellData[sheetName] = {}
+      totalSheet+=1;
+      lastlyAddedSheet+=1;
+      selectedSheet = sheetName
+      $('.sheet_tab_container').append(`<div class="sheet_tab sheet_selected">${sheetName} </div>`)
+      //adding event listner on new sheet --START--
+     addSheetEvent()
+    //adding event listner on new sheet --END--  
+    })
+   //Add Sheet --END-- 
+
+  //scrolling sheet tb container --START--
+   $('.scroller_icon').click(function(e){
+     // console.log("hh",e)
+     let sheet_scroll = document.getElementsByClassName('sheet_tab_container')[0]
+     if(e.target.id === "scroller_icon_left"){
+        // console.log("scrollLeft : ",sheet_scroll.scrollLeft)
+       sheet_scroll.scrollLeft = sheet_scroll.scrollLeft - 65
+     }
+     if(e.target.id === "scroller_icon_right"){
+       // console.log("scrollRight : ",sheet_scroll.scrollLeft)
+       sheet_scroll.scrollLeft = sheet_scroll.scrollLeft + 65
+     }
+   })
+
+  //scrolling sheet tb container --END--
+
+  // Selecting a Particular sheet --START--
+  addSheetEvent()
+  // Selecting a Particular sheet --END--
+
+  //renaming sheet --START--
+  $('.sheet_options_modal').click(function(e){
+    if(e.target.className.includes('sheet_rename')){
+      $('.sheet_rename_modal').css('visibility','visible')
+      
+    }
+    if(e.target.className.includes('sheet_delete')){
+      const result = deleteSheet()
+      selectedSheet = result===""?selectedSheet:result
+      totalSheet-=1;
+      lastlyAddedSheet-=1;
+    }
+  })
+
+   $('.action_buttons').click(function(e){
+       
+     if(e.target.className.includes('rename_button') && $('.new_sheet_name').val()){
+       let new_name = $('.new_sheet_name').val().trim().toLowerCase().replaceAll(" ","-")
+       if(Object.keys(cellData).includes(new_name)){
+         alert("Sheet Already Exists!")
+       }else{
+       $('.new_sheet_name').val("")
+       let same = $(renameSheetSelected).hasClass('sheet_selected')
+       let old_name = $(renameSheetSelected).text().trim().toLowerCase()
+       const result =   renameSheet(old_name,new_name,same) 
+
+       selectedSheet = result===""?selectedSheet:result;
+
+       // console.log("sheet renamed : ",selectedSheet)
+
+       $(renameSheetSelected).text(new_name)
+       
+       $('.sheet_rename_modal').css('visibility','hidden')
+       } 
+     }
+     
+     if(e.target.className.includes('cancel_button')){
+       $('.sheet_rename_modal').css('visibility','hidden')
+     }
+   })
+  
+  //renaming sheet --END--
+
+  //adding copying cells feature --START--
+  $('.icon_copy').click(function(){
+    if(selectedCells.length>0)selectedCells = []
+    $('.input_cell.selected').each(function(){
+      const {row,col} = getRowCol(this)
+      selectedCells.push([row,col])
+       // console.log(selectedCells)
+      
+    })
+  })
+  
+  //adding copying cells feature --END--
+
+  //adding paste cells feature --START--
+  $('.icon_paste').click(paste)
+
+  $('.icon_cut').click(function(){
+     $('.input_cell.selected').each(function(){
+      const {row,col} = getRowCol(this)
+      selectedCells.push([row,col])
+    })
+    cut = true
+    // for(let cell of selectedCells){
+    //   deleteCell(cell[0],cell[1],selectedSheet)
+    // }
+  })
+
+  //adding paste cells feature --END--
 
 $('.align_icon').click(function(){
   $('.align_icon.selected').removeClass('selected')
@@ -65,7 +237,7 @@ $('.style_icon').click(function(){
 $('.input_cell').click(function(e){
   if(e.ctrlKey){
     $(this).addClass('selected')
-    const {row,col,code} = getRowCol(e)
+    const {row,col} = getRowCol(this)
     // console.log(row,col,code)
     const topCellSelected = $(`#row-${row-1}-col-${col}`)
     const bottomCellSelected = $(`#row-${row+1}-col-${col}`)
@@ -100,6 +272,8 @@ $('.input_cell').click(function(e){
   $('.input_cell.selected').attr("contenteditable","false")
   selectCell(this)
   }
+  changeHeader(this,selectedSheet)
+  // console.log("selected sheet : ",selectedSheet)
 })
 
 $('.input_cell').dblclick(function(){
@@ -113,8 +287,7 @@ $('.input_cell').dblclick(function(){
     $(".column_name_container").scrollLeft(this.scrollLeft);
     $(".row_name_container").scrollTop(this.scrollTop);
   })
- 
-
+   
 
 
 
@@ -122,6 +295,66 @@ $('.input_cell').dblclick(function(){
 
   
 })
+
+function paste(){
+    if(selectedCells.length===0){
+      alert("Select cell for paste!")
+      return ;
+    }
+    EmptySheet(selectedSheet)
+    const {row,col} = getRowCol($('.input_cell.selected')[0])
+    const rowDistance = row - selectedCells[0][0]
+    const colDistance = col - selectedCells[0][1]
+    for(let cell of selectedCells){
+      const newRow = cell[0] + rowDistance
+      const newCol = cell[1] + colDistance
+      if(!cellData[selectedSheet][newRow]){
+        cellData[selectedSheet][newRow] = {}
+      }
+
+      if(cellData[selectedSheet][cell[0]]!==undefined && cellData[selectedSheet][cell[0]][cell[1]]!==undefined){
+        cellData[selectedSheet][newRow][newCol] = {...cellData[selectedSheet][cell[0]][cell[1]]}
+      }
+      
+      if(cut && cellData[selectedSheet][cell[0]]!==undefined && cellData[selectedSheet][cell[0]][cell[1]]!==undefined){
+        delete cellData[selectedSheet][cell[0]][cell[1]];
+        if(Object.keys(cellData[selectedSheet][cell[0]]).length===0){
+          delete cellData[selectedSheet][cell[0]]
+        }
+      }
+    }
+    loadSheet(selectedSheet)
+    if(cut){
+      selectedCells = []
+      cut = false
+    }
+}
+
+function addSheetEvent(){
+      $('.sheet_tab.sheet_selected').click(function(e){
+     if(!$(this).hasClass('sheet_selected')){
+       selectSheet(this)
+     }
+  });
+
+  $('.sheet_tab.sheet_selected').contextmenu(function(e){
+    e.preventDefault()
+    // console.log(e)
+    selectSheet(e.target)
+    $('.sheet_options_modal').css("left",`${e.clientX}px`)
+    $('.sheet_options_modal').css("visibility","visible")
+    renameSheetSelected = e.target
+  });
+  
+}
+
+ function selectSheet(elem){
+  $('.sheet_tab.sheet_selected').removeClass('sheet_selected')
+  $(elem).addClass('sheet_selected')
+   EmptySheet(selectedSheet)
+   selectedSheet = $.trim($(elem).text().toLowerCase())
+   loadSheet(selectedSheet)
+}
 
 function getColumnName(n)
 {
@@ -138,3 +371,8 @@ function getColumnName(n)
   }
 	return result;
 }
+
+//event listner on window for removing sheet options modal
+$('.container').click(function(){
+  $('.sheet_options_modal').css("visibility","hidden")
+})
--- util_function.js
+++ util_function.js
@@ -17,14 +17,199 @@ export function selectCell(c){
 }
 
 export function getRowCol(c){
-let {row,col,code} = c.target.dataset;
+  // console.log(c)
+  // console.log($('.input_cell.selected'))
+  let cell = $('.input_cell.selected')
+  // console.log("c : ",cell.attr('data-col'),cell.attr('data-row'))
+  let row,col;
+  if(c){
+   row  = $(c).attr('data-row')
+   col = $(c).attr('data-col')
   row = Number.parseInt(row)
   col = Number.parseInt(col)
-  return {row,col,code}
+  return {row,col}
+  }else{
+    let row = Number.parseInt(cell.attr('data-row'))
+    let col = Number.parseInt(cell.attr('data-col'))
+    return {row,col}
+  }
 }
 
-export function updateCell(property,value){
+// cell props
+export const cellProps = {
+  "width":"100",
+  "height":"25",
+  "color":"black",
+  "background-color":'white',
+  "font-style":'',
+  'text-decoration':'',
+  'font-weight':'',
+  'text':'',
+  'font-size':'16',
+  'font-family':'Poppins',
+  'text-align':'left'
+}
+
+export const cellData = {
+  "sheet-1":{}
+}
+
+export function updateCell(property,value,selectedSheet,defaultProp){
+  // console.log("updateCell",selectedSheet)
     $('.input_cell.selected').each(function(){
       $(this).css(property,value)
+      let {row,col} = getRowCol(this)
+      if(cellData[selectedSheet][row]){
+         if(cellData[selectedSheet][row][col]){
+             cellData[selectedSheet][row][col][property] = value
+         }else{
+            cellData[selectedSheet][row][col] = {...cellProps}
+           cellData[selectedSheet][row][col][property] = value
+         }   
+      }else{
+        cellData[selectedSheet][row] = {}
+        cellData[selectedSheet][row][col] = {...cellProps}
+        cellData[selectedSheet][row][col][property] = value
+      }
+
+      if(defaultProp && JSON.stringify(cellData[selectedSheet][row][col])===JSON.stringify(cellProps)){
+          //then delete that cell
+        delete cellData[selectedSheet][row][col]
+
+        //check row is empty or not
+        if(Object.keys(cellData[selectedSheet][row]).length==0){
+          delete cellData[selectedSheet][row];
+        }
+        
+      }
+      
     })
+
+  // console.log("cell data : ",cellData)
+}
+
+export function changeHeader(c,selectedSheet){
+  const {row,col} = getRowCol(c)
+  let cell_info = cellProps;
+  if(cellData[selectedSheet][row] && cellData[selectedSheet][row][col]){
+    cell_info = cellData[selectedSheet][row][col]
+    // console.log(cell_info)
+  }
+  
+cell_info['font-weight']?$('.icon_bold').addClass('selected'):$('.icon_bold').removeClass('selected')
+cell_info['font-style']?$('.icon_italic').addClass('selected'):$('.icon_italic').removeClass('selected')
+cell_info['text-decoration']?$('.icon_underline').addClass('selected'):$('.icon_underline').removeClass('selected')
+
+const alignment = cell_info["text-align"]  
+$('.align_icon.selected').removeClass('selected')
+$('.align_icon.icon_'+alignment).addClass('selected')
+
+// console.log(cell_info)
+$('.selector.font_family').val(cell_info["font-family"])
+$('.selector.font_size').val(cell_info["font-size"].split("px")[0]) 
+  
+}  
+
+export function EmptySheet(selectedSheet){
+  let sheetInfo = cellData[selectedSheet]
+  // console.log("sheetInfo : ",sheetInfo)
+  for(let row of Object.keys(sheetInfo)){
+    // console.log("row : ",row,cellProps)
+    for(let col of Object.keys(sheetInfo[row])){
+      // console.log("col : ",col,cellProps)
+      $(`#row-${row}-col-${col}`).text("")
+      $(`#row-${row}-col-${col}`).css("background-color","white")
+      $(`#row-${row}-col-${col}`).css("color","black")
+      $(`#row-${row}-col-${col}`).css("text-align","left")
+      $(`#row-${row}-col-${col}`).css("font-weight","")
+      $(`#row-${row}-col-${col}`).css("font-style","")
+      $(`#row-${row}-col-${col}`).css("text-decoration","")
+      $(`#row-${row}-col-${col}`).css("font-family","Poppins")
+      $(`#row-${row}-col-${col}`).css("font-size","16px")
+    }
+  }
+
+    // console.log("empty sheet : ",cellData)
+}
+
+export function loadSheet(selectedSheet){
+   let sheetInfo = cellData[selectedSheet]
+  // console.log("loading sheet",sheetInfo)
+  for(let row of Object.keys(sheetInfo)){
+    for(let col of Object.keys(sheetInfo[row])){
+      let cellInfo = sheetInfo[row][col]
+      // console.log("cell Info",sheetInfo)
+      $(`#row-${row}-col-${col}`).text(cellInfo['text'])
+      $(`#row-${row}-col-${col}`).css("background-color",cellInfo["background-color"])
+      $(`#row-${row}-col-${col}`).css('color',cellInfo['color'])
+      $(`#row-${row}-col-${col}`).css("text-align",cellInfo["text-align"])
+      $(`#row-${row}-col-${col}`).css("font-weight",cellInfo["font-weight"])
+      $(`#row-${row}-col-${col}`).css("font-style",cellInfo["font-style"])
+      $(`#row-${row}-col-${col}`).css("text-decoration",cellInfo["text-decoration"])
+      $(`#row-${row}-col-${col}`).css("font-family",cellInfo["font-family"])
+      $(`#row-${row}-col-${col}`).css("font-size",cellInfo["font-size"]+"px")
+    }
+  }
+
+    // console.log("empty sheet : ",cellData)
+}
+
+export function renameSheet(old_name,new_name,same){
+     console.log("new , old ,same : ",old_name,new_name,same)
+     console.log("before rename : ",cellData)
+     const sheet = cellData[old_name]
+     delete cellData[old_name]
+     cellData[new_name] = sheet
+     console.log("after rename : ",cellData)  
+     if(same){
+       return new_name
+     }
+  return ""
+}
+
+export function deleteSheet(){
+  // console.log("before deletion : ",cellData)
+  let childElem = $('.sheet_tab_container')[0].childElementCount
+  if(childElem===1){
+    alert('Default sheet can not be deleted!')
+    return ""
+  }else{
+    const prompt_ans = confirm("Are you sure !")
+    if(prompt_ans){
+       // console.log($('.sheet_tab_container')) 
+      const sheetName = $('.sheet_tab.sheet_selected').text().trim().toLowerCase()
+
+      //deleting from UI
+      $('.sheet_tab.sheet_selected').remove()
+
+      EmptySheet(sheetName)
+      
+      //deleting from cellData
+    delete cellData[sheetName]
+
+    const loadedSheet = $('.sheet_tab_container')[0].children[$('.sheet_tab_container')[0].childElementCount-1].innerText.trim().toLowerCase()
+
+     loadSheet(loadedSheet)  
+     
+      //selected new sheet
+$('.sheet_tab_container')[0].children[$('.sheet_tab_container')[0].childElementCount-1].classList.add('sheet_selected')
+      // console.log($('.sheet_tab_container')) 
+    // console.log("after deletion : ",cellData)  
+     return loadedSheet; 
+    }
+    
+  }
+   // console.log($('.sheet_tab_container')[0].childElementCount)
+}
+
+export function deleteCell(row,col,selectedSheet){
+      $(`#row-${row}-col-${col}`).text("")
+      $(`#row-${row}-col-${col}`).css("background-color","white")
+      $(`#row-${row}-col-${col}`).css("color","black")
+      $(`#row-${row}-col-${col}`).css("text-align","left")
+      $(`#row-${row}-col-${col}`).css("font-weight","")
+      $(`#row-${row}-col-${col}`).css("font-style","")
+      $(`#row-${row}-col-${col}`).css("text-decoration","")
+      $(`#row-${row}-col-${col}`).css("font-family","Poppins")
+      $(`#row-${row}-col-${col}`).css("font-size","16px") 
 }
